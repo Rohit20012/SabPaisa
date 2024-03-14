40c8e6510bf37337f3cb94349e87b1aadb880908
ROHIT <rohit@gmail.com>
2024-03-14 15:39:04 +0530

    first commit

---
+++ public/build/js/script.js
@@ -0,0 +1,120 @@
+// All javascript code in this project for now is just for demo DON'T RELY ON IT
+
+const random = (max = 100) => {
+  return Math.round(Math.random() * max) + 20
+}
+
+const randomData = () => {
+  return [
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+    random(),
+  ]
+}
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const cssColors = (color) => {
+  return getComputedStyle(document.documentElement).getPropertyValue(color)
+}
+
+const getColor = () => {
+  return window.localStorage.getItem('color') ?? 'cyan'
+}
+
+const colors = {
+  primary: cssColors(`--color-${getColor()}`),
+  primaryLight: cssColors(`--color-${getColor()}-light`),
+  primaryLighter: cssColors(`--color-${getColor()}-lighter`),
+  primaryDark: cssColors(`--color-${getColor()}-dark`),
+  primaryDarker: cssColors(`--color-${getColor()}-darker`),
+}
+
+const barChart = new Chart(document.getElementById('barChart'), {
+  type: 'bar',
+  data: {
+    labels: months,
+    datasets: [
+      {
+        data: randomData(),
+        backgroundColor: colors.primary,
+        hoverBackgroundColor: colors.primaryDark,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          gridLines: false,
+          ticks: {
+            beginAtZero: true,
+            stepSize: 50,
+            fontSize: 12,
+            fontColor: '#97a4af',
+            fontFamily: 'Open Sans, sans-serif',
+            padding: 10,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: false,
+          ticks: {
+            fontSize: 12,
+            fontColor: '#97a4af',
+            fontFamily: 'Open Sans, sans-serif',
+            padding: 5,
+          },
+          categoryPercentage: 0.5,
+          maxBarThickness: '20',
+        },
+      ],
+    },
+    cornerRadius: 2,
+    maintainAspectRatio: false,
+    legend: {
+      display: false,
+    },
+  },
+})
+
+const doughnutChart = new Chart(document.getElementById('doughnutChart'), {
+  type: 'doughnut',
+  data: {
+    labels: ['Net Banking', 'UPI'],
+    datasets: [
+      {
+        data: [5463, 1500], // Set specific values for Net Banking and UPI-02
+        backgroundColor: [colors.primary, 'orange'], // Change UPI color to orange
+        hoverBackgroundColor: colors.primaryDark,
+        borderWidth: 0,
+        weight: 0.5,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      position: 'bottom',
+    },
+
+    title: {
+      display: false,
+    },
+    animation: {
+      animateScale: true,
+      animateRotate: true,
+    },
+  },
+});
